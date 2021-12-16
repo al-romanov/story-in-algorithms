4881b91aa6b022c9dc40fcdd43277dd07ac78697
Al.Romanov <raa_2001@mail.ru>
2021-12-16 16:46:53 +0300

    First commit

---
+++ random-contraction.h
@@ -0,0 +1,8 @@
+#ifndef STORY_IN_ALGORITHMS_RANDOM_CONTRACTION_H_
+#define STORY_IN_ALGORITHMS_RANDOM_CONTRACTION_H_
+
+std::vector<std::list<size_t>> RandomContraction(const std::vector<std::list<size_t>> &adjacencyList) {
+    while (adjaacencyList.size())
+}
+
+#endif  // STORY_IN_ALGORITHMS_RANDOM_CONTRACTION_H_
